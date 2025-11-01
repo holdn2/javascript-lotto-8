c7ca89eb6f2c418837935c83e216bbcbeb696527
holdn2 <yoocy01@gmail.com>
2025-11-01 23:01:11 +0900

    feat(LottoDrawController): 구매 수량에 맞게 로또를 생성하고 결과를 출력하는 controller

---
+++ src/controller/LottoDrawController.js
@@ -0,0 +1,24 @@
+import outputView from '../view/OutputView.js';
+import { drawManyLotto } from '../service/lotto/LottoDrawService.js';
+
+export default class LottoDrawController {
+  #purchaseQuantity;
+
+  constructor(purchaseQuantity) {
+    this.#purchaseQuantity = purchaseQuantity;
+  }
+
+  run() {
+    outputView.printPurchaseQuantity(this.#purchaseQuantity);
+
+    const drawnLottos = this.#drawLottos();
+
+    outputView.printDrawLottos(drawnLottos);
+
+    return drawnLottos;
+  }
+
+  #drawLottos() {
+    return drawManyLotto(this.#purchaseQuantity);
+  }
+}
