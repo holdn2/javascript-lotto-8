e54eda389ed8f8542fd0a5f9aff3d2600aef6083
holdn2 <yoocy01@gmail.com>
2025-11-01 14:28:17 +0900

    feat(constant): 안내 및 에러 메시지 상수화

---
+++ src/constant/error.js
@@ -0,0 +1,14 @@
+export const ERROR_MESSAGE = Object.freeze({
+  INVALID_PURCHASE_AMOUNT:
+    '[ERROR] 로또 구입 금액은 1,000원 단위로만 입력 가능합니다.',
+  NEGATIVE_OR_ZERO_PURCHASE_AMOUNT:
+    '[ERROR] 구입 금액은 양의 정수로 입력해야 합니다.',
+  INVALID_LOTTO_NUMBER_RANGE:
+    '[ERROR] 로또 번호와 보너스 번호는 범위 내 숫자로만 입력 가능합니다. (범위: 1 ~ 45)',
+  INVALID_LOTTO_NUMBER_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  NOT_A_NUMBER: '[ERROR] 구입 금액과 보너스 번호는 숫자로만 입력 가능합니다.',
+  INVALID_CHARACTER:
+    "[ERROR] 로또 번호는 숫자 또는 쉼표(',')로만 입력 가능합니다.",
+  DUPLICATE_NUMBER:
+    '[ERROR] 로또 번호는 중복되는 숫자가 포함되지 않아야 합니다.',
+});
