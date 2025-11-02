246ff880c5a842ea0464e2f0251ea778164a7ac8
holdn2 <yoocy01@gmail.com>
2025-11-02 19:23:24 +0900

    refactor(validator): 입력 검증 함수 하나의 파일에서 관리

--- src/service/validator/validators.js
+++ src/service/validator/validators.js
@@ -1,5 +1,6 @@
 import { ERROR_MESSAGE } from '../../constant/error.js';
 import { NUMBER } from '../../constant/number.js';
+import { REGEX } from '../../constant/regex.js';
 
 export function validatePurchaseAmount(purchaseAmount) {
   if (isNaN(purchaseAmount)) throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
@@ -9,4 +10,15 @@ export function validatePurchaseAmount(purchaseAmount) {
 
   if (purchaseAmount % NUMBER.UNIT !== NUMBER.ZERO)
     throw new Error(ERROR_MESSAGE.INVALID_PURCHASE_AMOUNT);
+}
+
+export function validateInputWinningNumbers(inputWinningNumbers) {
+  if (!REGEX.VALID_CHARACTER.test(inputWinningNumbers)) {
+    throw new Error(ERROR_MESSAGE.INVALID_CHARACTER);
+  }
+  // TODO: 쉼표 연속이나 앞뒤가 숫자가 아니면 에러
+}
+
+export function validateInputBonusNumber(inputBonusNumber) {
+  if (isNaN(inputBonusNumber)) throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
 }
