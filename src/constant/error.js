6807497ec3c66c7ddfc99b4cc2a0e3420c045a7c
holdn2 <yoocy01@gmail.com>
2025-11-02 11:51:30 +0900

    style(error): 에러 메시지 수정

--- src/constant/error.js
+++ src/constant/error.js
@@ -4,11 +4,12 @@ export const ERROR_MESSAGE = Object.freeze({
   NEGATIVE_OR_ZERO_PURCHASE_AMOUNT:
     '[ERROR] 구입 금액은 양의 정수로 입력해야 합니다.',
   INVALID_LOTTO_NUMBER_RANGE:
-    '[ERROR] 로또 번호와 보너스 번호는 범위 내 숫자로만 입력 가능합니다. (범위: 1 ~ 45)',
+    '[ERROR] 로또 번호와 보너스 번호는 범위 내 정수로만 입력 가능합니다. (범위: 1 ~ 45)',
   INVALID_LOTTO_NUMBER_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
-  NOT_A_NUMBER: '[ERROR] 구입 금액과 보너스 번호는 숫자로만 입력 가능합니다.',
+  NOT_A_NUMBER:
+    '[ERROR] 구입 금액과 보너스 번호는 양의 정수로만 입력 가능합니다.',
   INVALID_CHARACTER:
-    "[ERROR] 로또 번호는 숫자 또는 쉼표(',')로만 입력 가능합니다.",
+    "[ERROR] 로또 번호는 양의 정수 또는 쉼표(',')로만 입력 가능합니다.",
   DUPLICATE_LOTTO_NUMBER:
     '[ERROR] 로또 번호는 중복되는 숫자가 포함되지 않아야 합니다.',
   BONUS_DUPLICATED_WITH_LOTTO:
