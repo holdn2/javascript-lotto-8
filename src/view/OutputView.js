e5c4bc0ae971c3deee133d1de203c9bdf9fd0530
holdn2 <yoocy01@gmail.com>
2025-11-02 22:41:16 +0900

    feat(OutputView): 결과 출력 함수 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,11 +5,35 @@ import { OUTPUT_MESSAGE } from '../constant/inform.js';
 const outputView = {
   printPurchaseQuantity(purchaseQuantity) {
     Console.print(
-      `${MARK.NEW_LINE}${purchaseQuantity}${OUTPUT_MESSAGE.PURCHASE_QUANTITY}`,
+      `${MARK.NEW_LINE + purchaseQuantity + OUTPUT_MESSAGE.PURCHASE_QUANTITY}`,
     );
   },
+
   printDrawLottos(drawnLottos) {
-    drawnLottos.forEach((lotto) => Console.print(lotto));
+    drawnLottos.forEach((lotto) => {
+      const lottoString = `[${lotto.join(', ')}]`;
+      Console.print(lottoString);
+    });
+  },
+
+  printResultInform() {
+    Console.print(OUTPUT_MESSAGE.WINNING_STATISTICS);
+  },
+
+  printMatchesInform(statistics) {
+    const matchesInform = [
+      `${OUTPUT_MESSAGE.THREE_MATCHES}${statistics.threeMatches}${OUTPUT_MESSAGE.COUNTER}`,
+      `${OUTPUT_MESSAGE.FOUR_MATCHES}${statistics.fourMatches}${OUTPUT_MESSAGE.COUNTER}`,
+      `${OUTPUT_MESSAGE.FIVE_MATCHES}${statistics.fiveMatches}${OUTPUT_MESSAGE.COUNTER}`,
+      `${OUTPUT_MESSAGE.FIVE_MATCHES_WITH_BONUS}${statistics.fiveMatchesWithBonus}${OUTPUT_MESSAGE.COUNTER}`,
+      `${OUTPUT_MESSAGE.SIX_MATCHES}${statistics.sixMatches}${OUTPUT_MESSAGE.COUNTER}`,
+    ].join('\n');
+
+    Console.print(matchesInform);
+  },
+
+  printTotalRateOfReturn(totalRateOfReturn) {
+    Console.print(OUTPUT_MESSAGE.TOTAL_RATE_OF_RETURN(totalRateOfReturn));
   },
 };
 
