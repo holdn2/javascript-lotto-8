1142df4323dd846537286527247bb64693768c25
holdn2 <yoocy01@gmail.com>
2025-11-03 02:27:08 +0900

    test(LottoCheckService): checkLottoMatch에 대한 테스트

---
+++ src/service/lotto/LottoCheckService.test.js
@@ -0,0 +1,36 @@
+import { checkLottoMatch } from './LottoCheckService.js';
+import Lotto from '../../model/Lotto.js';
+import Bonus from '../../model/Bonus.js';
+
+describe('checkLottoMatch', () => {
+  test('일치 개수에 따라 통계(statistics)가 올바르게 증가한다', () => {
+    // given
+    const statistics = {
+      sixMatches: 0,
+      fiveMatchesWithBonus: 0,
+      fiveMatches: 0,
+      fourMatches: 0,
+      threeMatches: 0,
+    };
+
+    const drawnLottos = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 8, 10],
+    ];
+
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const bonus = new Bonus(7, lotto);
+    const winningLotto = { lotto, bonus };
+
+    checkLottoMatch(statistics, drawnLottos, winningLotto);
+
+    expect(statistics).toEqual({
+      sixMatches: 1,
+      fiveMatchesWithBonus: 1,
+      fiveMatches: 0,
+      fourMatches: 1,
+      threeMatches: 0,
+    });
+  });
+});
