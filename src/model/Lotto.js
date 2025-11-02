c6d4867609f8702162a009ea60f9b17f42537940
holdn2 <yoocy01@gmail.com>
2025-11-02 20:32:41 +0900

    feat(Lotto): 검증 및 함수 추가

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -1,20 +1,28 @@
-import { ERROR_MESSAGE } from '../constant/error.js';
+import {
+  validateLottoNumberDuplication,
+  validateLottoNumbersLength,
+  validateNumberRange,
+} from '../service/validator/validators.js';
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers;
+    this.#validate();
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error(ERROR_MESSAGE.INVALID_NUMBER_LENGTH);
-    }
+  #validate() {
+    validateLottoNumbersLength(this.#numbers);
+    this.#numbers.forEach((number) => validateNumberRange(number));
+    validateLottoNumberDuplication(this.#numbers);
   }
 
-  // TODO: 추가 기능 구현
+  contains(number) {
+    return this.#numbers.includes(number);
+  }
+
+  // TODO: 숫자 배열을 받아서 일치하는 개수를 반환하는 함수
 }
 
 export default Lotto;
