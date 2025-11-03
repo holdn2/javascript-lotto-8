a3102b9a375144fc95120a0446f24a879b3e8ca5
holdn2 <yoocy01@gmail.com>
2025-11-03 12:30:54 +0900

    refactor(constant): WINNING_AMOUNT 상수 사용하도록 수정

--- src/constant/inform.js
+++ src/constant/inform.js
@@ -1,3 +1,5 @@
+import { WINNING_AMOUNT } from './number.js';
+
 export const INPUT_MESSAGE = Object.freeze({
   PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
   WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
@@ -7,11 +9,11 @@ export const INPUT_MESSAGE = Object.freeze({
 export const OUTPUT_MESSAGE = Object.freeze({
   PURCHASE_QUANTITY: '개를 구매했습니다.',
   WINNING_STATISTICS: '\n당첨 통계\n---',
-  THREE_MATCHES: '3개 일치 (5,000원) - ',
-  FOUR_MATCHES: '4개 일치 (50,000원) - ',
-  FIVE_MATCHES: '5개 일치 (1,500,000원) - ',
-  FIVE_MATCHES_WITH_BONUS: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-  SIX_MATCHES: '6개 일치 (2,000,000,000원) - ',
+  THREE_MATCHES: `3개 일치 (${WINNING_AMOUNT.FIFTH_PLACE.toLocaleString()}원) - `,
+  FOUR_MATCHES: `4개 일치 (${WINNING_AMOUNT.FOURTH_PLACE.toLocaleString()}원) - `,
+  FIVE_MATCHES: `5개 일치 (${WINNING_AMOUNT.THIRD_PLACE.toLocaleString()}원) - `,
+  FIVE_MATCHES_WITH_BONUS: `5개 일치, 보너스 볼 일치 (${WINNING_AMOUNT.SECOND_PLACE.toLocaleString()}원) - `,
+  SIX_MATCHES: `6개 일치 (${WINNING_AMOUNT.FIRST_PLACE.toLocaleString()}원) - `,
   COUNTER: '개',
   TOTAL_RATE_OF_RETURN: (rateOfReturn) => `총 수익률은 ${rateOfReturn}%입니다.`,
 });
