f2cd2b4a95cb9ee08b9a6c64c528cd4d0514f53c
holdn2 <yoocy01@gmail.com>
2025-11-05 16:13:40 +0900

    refactor(App): 하나의 메서드 사용하도록 수정

--- src/App.js
+++ src/App.js
@@ -6,48 +6,22 @@ import BonusNumberController from './controller/BonusNumberController.js';
 import outputView from './view/OutputView.js';
 
 class App {
-  #purchaseAmount;
-  #lotto;
-  #bonus;
-
   async run() {
-    await this.#runPurchaseController();
-    const drawnLottos = new LottoDrawController(this.#purchaseAmount).run();
-    await this.#runWinningNumberController();
-    await this.#runBonusNumberController();
-    new ResultController(this.#purchaseAmount, drawnLottos, {
-      lotto: this.#lotto,
-      bonus: this.#bonus,
+    const purchaseAmount = await this.#runController(new PurchaseController());
+    const drawnLottos = new LottoDrawController(purchaseAmount).run();
+    const lotto = await this.#runController(new WinningNumberController());
+    const bonus = await this.#runController(new BonusNumberController(lotto));
+    new ResultController(purchaseAmount, drawnLottos, {
+      lotto,
+      bonus,
     }).run();
   }
 
-  async #runPurchaseController() {
+  async #runController(controller) {
     while (true) {
       try {
-        this.#purchaseAmount = await new PurchaseController().run();
-        break;
-      } catch (e) {
-        outputView.printError(e.message);
-      }
-    }
-  }
-
-  async #runWinningNumberController() {
-    while (true) {
-      try {
-        this.#lotto = await new WinningNumberController().run();
-        break;
-      } catch (e) {
-        outputView.printError(e.message);
-      }
-    }
-  }
-
-  async #runBonusNumberController() {
-    while (true) {
-      try {
-        this.#bonus = await new BonusNumberController(this.#lotto).run();
-        break;
+        const value = await controller.run();
+        return value;
       } catch (e) {
         outputView.printError(e.message);
       }
